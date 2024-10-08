df1e81db51c0a75455624f8c122b8979298d21fb
JayGregory <jgregory@neworbit.co.uk>
2024-10-08 13:32:09 +0100

    added submissions

---
+++ Hackathon/submissions/Team 6/Hackathon/Hackathon_Neworbit/Program.cs
@@ -0,0 +1,49 @@
+﻿using Hackathon_Neworbit;
+using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var builder = Host.CreateEmptyApplicationBuilder(new HostApplicationBuilderSettings()
+{
+    Args = args
+} );
+
+
+var kernelBuilder = Kernel.CreateBuilder();
+kernelBuilder.AddAzureOpenAIChatCompletion("test", "https://2024-09-11-openai.openai.azure.com/", "6b294e0dd8284dd38fafd4bf5e519ca0", modelId: "gpt-4o");
+kernelBuilder.Plugins.AddFromType<LocationPlugin>();
+kernelBuilder.Plugins.AddFromType<SpeechPlugin>();
+
+var kernel = kernelBuilder.Build();
+
+
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new OpenAIPromptExecutionSettings()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+};
+
+var chatHistory = new ChatHistory();
+
+chatHistory.AddSystemMessage(
+    "You are a story telling AI." +
+    "You will be telling a bed time story to children. " +
+    "You will need to ask the parents how many children there." +
+    "You should also ask the parents what the kids interests are." +
+    "You should check for favourite locations and include only one of the locations but do not ask the user about this." +
+    "The story should have some funny aspects to it. " +
+    "Limit the story to 2 sentences." +
+    "Read out loud");
+
+while (true)
+{
+    var response = await chat.GetChatMessageContentsAsync(chatHistory, executionSettings: openAIPromptExecutionSettings, kernel: kernel);
+    var lastMessage = response.Last();
+    Console.WriteLine(lastMessage);
+
+    var prompt = Console.ReadLine();
+    chatHistory.AddUserMessage(prompt!);
+}
+
---
+++ Hackathon/submissions/Team 6/Hackathon/Hackathon_Neworbit/SpeechPlugin.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace Hackathon_Neworbit
+{
+    public class SpeechPlugin
+    {
+        private readonly SpeechSynthesizer _speechSynthesizer;
+
+        public SpeechPlugin()
+        {
+            var speechConfig = SpeechConfig.FromSubscription("e5a804c3a8c742b2b715916680756ca4", "uksouth");
+            speechConfig.SetProperty(PropertyId.SpeechServiceResponse_PostProcessingOption, "TrueText");
+            speechConfig.SpeechSynthesisVoiceName = "en-US-JennyNeural";
+            var audioOutputConfig = AudioConfig.FromDefaultSpeakerOutput();
+            
+            _speechSynthesizer = new SpeechSynthesizer(speechConfig, audioOutputConfig);
+        }
+
+        [KernelFunction("ReadOutLoud")]
+        [Description("Respond")]
+        [return: Description("Read out loud")]
+        public void Respond(string message)
+        {
+            // Check if we have a message to speak
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                // Speak the SSML
+                _speechSynthesizer.SpeakTextAsync(message);
+            }
+        }
+    }
+}
